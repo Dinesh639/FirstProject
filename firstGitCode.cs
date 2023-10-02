743c42c07c4fe22a556c99ab3bc2e73dce20bd1e
Dinesh Kumar Kasi <dkasi@oeconnection.com>
2023-10-02 19:08:00 +0530

    My first git commit

---
+++ firstGitCode.cs
@@ -0,0 +1,11 @@
+using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the URL :");
+            string text = Console.ReadLine();
+            Console.WriteLine("Text from user :"+text);
+            Console.WriteLine("Regex Text :"+regexText);
+        }
+    }
