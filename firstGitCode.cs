326335af7147a52749f7ce85f62331ae201339dd
Dinesh639 <146737135+Dinesh639@users.noreply.github.com>
2023-10-02 19:39:28 +0530

    Removed commented line conflicts

--- firstGitCode.cs
+++ firstGitCode.cs
@@ -6,8 +6,6 @@ using System;
             Console.WriteLine("Enter the URL :");
             string text = Console.ReadLine();
             Console.WriteLine("Text from user :");
-            //Console.WriteLine("Text from user :");
             Console.WriteLine("Regex Text :"+regexText);
-            //commented the lines
         }
     }
